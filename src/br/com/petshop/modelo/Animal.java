28f8d6759f13da7b00205bb53aab7047bfcc033e
mariocyreno <mario_cyreno@hotmail.com>
2016-07-30 12:18:31 -0300

    Classe Animal e TipoAnimal Completo

--- src/br/com/petshop/modelo/Animal.java
+++ src/br/com/petshop/modelo/Animal.java
@@ -1,5 +1,34 @@
 package br.com.petshop.modelo;
 
 public class Animal {
-
+	private Integer id;
+	private String Nome;
+	private Integer id_Dono;
+	private Integer id_Tipo_Animal;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public Integer getId_Dono() {
+		return id_Dono;
+	}
+	public void setId_Dono(Integer id_Dono) {
+		this.id_Dono = id_Dono;
+	}
+	public Integer getId_Tipo_Animal() {
+		return id_Tipo_Animal;
+	}
+	public void setId_Tipo_Animal(Integer id_Tipo_Animal) {
+		this.id_Tipo_Animal = id_Tipo_Animal;
+	}
+	
+	
 }
--- src/br/com/petshop/modelo/TipoAnimal.java
+++ src/br/com/petshop/modelo/TipoAnimal.java
@@ -1,5 +1,20 @@
 package br.com.petshop.modelo;
 
 public class TipoAnimal {
+	private Integer Tipo_Animal_id;
+	private String Tipo;
+	public Integer getTipo_Animal_id() {
+		return Tipo_Animal_id;
+	}
+	public void setTipo_Animal_id(Integer tipo_Animal_id) {
+		Tipo_Animal_id = tipo_Animal_id;
+	}
+	public String getTipo() {
+		return Tipo;
+	}
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+
 
 }
