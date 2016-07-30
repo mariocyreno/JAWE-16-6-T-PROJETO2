9b54f6a3f51cfaf733d9facc94040110b5567d71
mariocyreno <mario_cyreno@hotmail.com>
2016-07-30 11:36:12 -0300

    Classe ConnectionFactory criada

--- src/br/com/petshop/util/ConnectionFactory.java
+++ src/br/com/petshop/util/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package br.com.petshop.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
 
+		public Connection getConnection(){
+			Connection con = null;
+				try {
+					con = DriverManager.getConnection ("jdbc:mysql://localhost/patshop", "root", "");
+					System.out.println("Conexão Realizada com Sucesso");
+					
+				} catch (SQLException e) {
+					System.err.println("Conexão não realizada");
+				}
+					
+			return con;	
+				
+			}
 }
