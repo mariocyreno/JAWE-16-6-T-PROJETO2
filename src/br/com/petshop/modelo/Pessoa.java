61354056d79c586ec6123973762851c3ca5e0d60
mariocyreno <mario_cyreno@hotmail.com>
2016-07-30 11:52:54 -0300

    Classe Connection  Factory e Pessoa Criadas

--- src/br/com/petshop/modelo/Pessoa.java
+++ src/br/com/petshop/modelo/Pessoa.java
@@ -2,4 +2,43 @@ package br.com.petshop.modelo;
 
 public class Pessoa {
 
+	private int id;
+	private String Nome;
+	private String Cpf;
+	private String Telefone;
+	private String Email;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCpf() {
+		return Cpf;
+	}
+	public void setCpf(String cpf) {
+		Cpf = cpf;
+	}
+	public String getTelefone() {
+		return Telefone;
+	}
+	public void setTelefone(String telefone) {
+		Telefone = telefone;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+	
 }
